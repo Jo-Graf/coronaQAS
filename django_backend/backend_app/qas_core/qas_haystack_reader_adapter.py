84c8ae7507b011c6510b4af8027ce0bd754280e1
Gino Lancho <gino.lancho@tu-dresden.de>
2020-09-16 18:53:12 +0200

    restructuring

--- django_backend/backend_app/qas_core/qas_haystack_reader_adapter.py
+++ django_backend/backend_app/qas_core/qas_haystack_reader_adapter.py
@@ -1,6 +1,7 @@
 from typing import List
 from haystack.reader.base import BaseReader
 
+from backend_app.qas_core.qas_answer import QASAnswer
 from backend_app.qas_core.qas_database import QASDatabase
 from backend_app.qas_core.qas_document import QASDocument
 from backend_app.qas_core.qas_haystack_database_adapter import QASHaystackDatabaseAdapter
@@ -12,3 +13,17 @@ class QASHaystackReaderAdapter(QASReaderVariant):
 
     def __init__(self, reader:  BaseReader = None):
         self.__reader = reader
+
+    def read(self, query: str, data: List[QASDocument]) -> List[QASAnswer]:
+        docs = self.__reader.predict(query, data)
+
+        qas_docs = []
+
+        # TODO: check adjustment for answer type
+        '''
+        for doc in docs:
+            qas_doc = QASDocument.doc_to_qas_doc(doc)
+            qas_docs.append(qas_doc)
+        '''
+
+        return docs
--- django_backend/backend_app/qas_core/test/qas_reader_test.py
+++ django_backend/backend_app/qas_core/test/qas_reader_test.py
@@ -1,29 +1,42 @@
 from haystack.database.elasticsearch import ElasticsearchDocumentStore
+from haystack.reader.farm import FARMReader
 from haystack.retriever.sparse import ElasticsearchRetriever
 
 from backend_app.qas_core.qas_database import QASDatabase
 from backend_app.qas_core.qas_got_data_loader import QASGOTDataLoaderVariant
 from backend_app.qas_core.qas_haystack_database_adapter import QASHaystackDatabaseAdapter
+from backend_app.qas_core.qas_haystack_reader_adapter import QASHaystackReaderAdapter
 from backend_app.qas_core.qas_haystack_retriever_adapter import QASHaystackRetrieverAdapter
+from backend_app.qas_core.qas_reader import QASReader
 from backend_app.qas_core.qas_retriever import QASRetriever
 
-print('### qas retriever test start ###')
 
+print('### qas reader test start ###')
+# variable
 dir_path = "/Users/Gino/Belegarbeit/django_backend/backend_app/data/article_txt_got"
 url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt.zip"
 question = 'Who is the mother of Sansa Stark?'
+model_name = 'deepset/roberta-base-squad2'
 
+# loader
 loader = QASGOTDataLoaderVariant(url, dir_path)
 
-document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document")
+# database
+document_store = ElasticsearchDocumentStore(host='localhost', username='', password='', index='document')
 database_variant = QASHaystackDatabaseAdapter(document_store=document_store)
 database = QASDatabase(variant=database_variant, loader=loader)
 database.load_data()
 
+# retriever
 haystack_retriever = ElasticsearchRetriever(None)
 retriever_variant = QASHaystackRetrieverAdapter(retriever=haystack_retriever)
 retriever = QASRetriever(variant=retriever_variant)
-result = retriever.retrieve(question, database)
-print(result[0])
+retrieved_docs = retriever.retrieve(question, database)
+
+# reader
+haystack_reader = FARMReader(model_name_or_path=model_name, use_gpu=False)
+reader_variant = QASHaystackReaderAdapter(reader=haystack_reader)
+reader = QASReader(variant=reader_variant)
+answers = reader.read(question, retrieved_docs)
 
 print('### qas retriever test end ###')
